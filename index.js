619e232c6849d2b02dae9484ad20aff91c463c94
Kesban <k.ahmedowa94@gmail.com>
2022-07-18 17:07:15 +0200

    Projekt geändert

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-function run(){
+/*function run(){
     let tagen = document.querySelector('#tagen').value;
     let person = document.querySelector('#person').value;
 
@@ -11,6 +11,26 @@ function run(){
     preis = (tagen * 50) + (person * 10) + ((withBreakfast * 8)* person);
 
     withBreakfast == true ? result.innerHTML = `Insgesamt ${preis} €`: result.innerHTML = `Insgesamt ${preis} €`;
+};*/
+
+const preisProNacht = 50;
+const jedePerson = 10;
+const frühstückPreis = 8;
+
+function run() {
+  const nacht = document.querySelector("#tagen").value;
+  const guest = document.querySelector("#person").value;
+  const mitFrühstück = document.querySelector("#essen").checked;
+
+  document.querySelector("#result").innerHTML="Insgesamt: " + calc (nacht,guest,mitFrühstück) + " €";
 };
 
-
+function calc(nacht, guest, mitFrühstück) {
+    let preis = (preisProNacht + jedePerson * (guest - 1)) * nacht;
+    if (mitFrühstück == true) {
+        preis = preis + frühstückPreis * guest * nacht;
+    } else {
+        preis       
+    }
+    return preis;
+};
